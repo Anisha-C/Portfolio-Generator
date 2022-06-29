0a176feb10f2fa413f7f11b05a415b57dbfa7080
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-06-29 19:28:50 -0400

    Added validation for prompts

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username(Required)',
+      validate: githubUsernameInput => {
+        if (githubUsernameInput) {
+          return true;
+        } else {
+          console.log('Please enter your username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -38,12 +54,28 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project?(Required)',
+      validate: projectNameInput => {
+        if (projectNameInput) {
+          return true;
+        } else {
+          console.log('Please enter your project name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter the description of the project!');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -54,7 +86,15 @@ Add a New Project
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('Please enter the link for the project!');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
@@ -69,15 +109,15 @@ Add a New Project
       default: false
     }
   ])
-  .then(projectData => {
-    portfolioData.projects.push(projectData);
-    if (projectData.confirmAddProject) {
-      return promptProject(portfolioData);
-    } else {
-      return portfolioData;
-    }
-  });
-  
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+
 };
 
 promptUser()
@@ -86,7 +126,7 @@ promptUser()
     console.log(portfolioData);
   });
 
-  
+
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
